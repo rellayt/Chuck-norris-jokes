a8de39ab05706fea5e6c6f59d0c3344d4e923d98
RLT <rellayt09s@gmail.com>
2021-05-29 21:13:56 +0200

    Abstract controls are now available from shared component logic

--- src/app/jokes/quantity-input/quantity-input.component.ts
+++ src/app/jokes/quantity-input/quantity-input.component.ts
@@ -1,20 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ControlContainer } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlContainer, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-quantity-input',
   templateUrl: './quantity-input.component.html',
   styleUrls: ['./quantity-input.component.scss']
 })
-export class QuantityInputComponent {
 
-  constructor(public controlContainer: ControlContainer) { }
+export class QuantityInputComponent implements OnInit {
+
   @Input() controlName: string
   @Input() hasError: boolean = false
 
   @Output() onValueChange = new EventEmitter<number>();
+  formControl: FormControl
 
   handleQuantityChange(value: number): void {
     this.onValueChange.emit(value)
   }
+
+  ngOnInit(): void {
+    this.formControl = this.controlContainer.control.get(this.controlName) as FormControl
+  }
+
+  constructor(private controlContainer: ControlContainer) { }
 }
